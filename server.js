991abcad709a0922291a02d6afd4bc0ca76c9c27
RamjeeC <112837025+RamjeeC@users.noreply.github.com>
2024-03-15 14:13:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import Connection from './config/db.Connection.js'
+import Routes from './Routes/user.Routes.js'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express()
+dotenv.config()
+
+app.use(express.json())
+app.use(cors({
+  origin: ['https://keymenadvisory.com', 'http://13.201.5.3:80']
+}))
+
+
+// Set up the static files serving for the client build directory
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.join(__dirname, './client/build')))
+
+// Define routes
+app.use('/api', Routes)
+
+// Serve index.html for all other routes (React Router)
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+})
+
+Connection()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
